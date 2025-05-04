ba6d7f45978e4b6843427ef1fbc4a7b47accc7ff
Rémi <glatech@gmx.fr>
2025-05-04 18:38:54 +0200

    Fix

--- compose.sh
+++ compose.sh
@@ -4,4 +4,4 @@ COMPOSE_FILE=docker-compose.yml
 CUSTOM_COMPOSE_FILE=docker-compose-custom.yml 
 
 env $(cat .env .env.custom | grep -v '^#' | xargs) \
-    docker compose -f ${COMPOSE_FILE} -f ${CUSTOM_COMPOSE_FILE} --project-name elk $@
+    docker-compose -f ${COMPOSE_FILE} -f ${CUSTOM_COMPOSE_FILE} --project-name elk $@
